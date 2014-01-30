553db3323c9354fd8df6276fb671ca37984306f4
James Brumond <kbjr14@gmail.com>
2014-01-30 10:01:54 -0800

    working on test specs (class)

--- tests/specs/class.js
+++ tests/specs/class.js
@@ -9,6 +9,49 @@ describe('Class', function() {
 	});
 	
 	describe('Class.extend', function() {
-		// 
+		var Sub, SubSub, sub, subSub;
+
+		beforeEach(function() {
+			Sub = Class.extend({
+				prop: 'foo',
+				method: function() {
+					return 'foo';
+				}
+			});
+
+			SubSub = Sub.extend({
+				foo: 'bar',
+				prop: 'baz'
+			});
+
+			sub = new Sub();
+			subSub = new SubSub();
+		});
+
+		it('should create a new subclass', function() {
+			expect(typeof Sub).toBe('function');
+			expect(sub instanceof Sub).toBeTruthy();
+		});
+
+		it('should set the methods and properties to the new subclass', function() {
+			expect(Sub.prototype.prop).toBe('foo');
+			expect(Sub.prototype.method()).toBe('foo');
+			expect(sub.prop).toBe('foo');
+			expect(sub.method()).toBe('foo');
+		});
+
+		it('should create extendable subclasses', function() {
+			expect(Sub.extend).toBe(Class.extend);
+			expect(SubSub.prototype.foo).toBe('bar');
+			expect(SubSub.prototype.prop).toBe('baz');
+			expect(subSub.method()).toBe('foo');
+		});
+
+		it('should set up correct inheritence for the subclass', function() {
+			expect(Sub.prototype.constructor).toBe(Class);
+			expect(SubSub.prototype.constructor).toBe(Sub);
+			expect(subSub instanceof Sub).toBeTruthy();
+			expect(subSub instanceof Class).toBeTruthy();
+		});
 	});
 });
